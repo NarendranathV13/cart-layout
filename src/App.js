5a91454052fd46aa3f83edb14b696e0de0bd1204
NarendranathV13 <narendranath.v@arkinfotec.com>
2023-09-13 12:49:50 +0530

    login validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 // import Demo2 from './Components/demo2';
-// import Login from './Pages/Login/Login';
+import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 function App() {
   return (
     <div className="App">
-      <Register />
-      {/* <Login /> */}
+      {/* <Register /> */}
+      <Login />
       {/* <Demo2 /> */}
     </div>
   );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,73 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { Contain, LoginButton, InputField, Heading, LoginContainer, WelcomeText } from "../../StyledComponent/LoginStyle"; // Update the import path
-
 const Login = () => {
-  return (
-    <Contain>
-      <LoginContainer>
-        <Heading>Welcome</Heading>
-        <WelcomeText>Login</WelcomeText>
-        <div className="login-form"> {/* If you want to add custom classes, do it here */}
-          <InputField type="email" placeholder="user email" />
-          <InputField type="password" placeholder="Password" />
-          <LoginButton type="button">Login</LoginButton>
-          <p className="text-center">New user?</p>
-          <LoginButton type="button">Register</LoginButton>
-        </div>
-      </LoginContainer>
-    </Contain>
-  );
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email("Invalid email").required("Email is required"),
+        password: Yup.string()
+            .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                'Password must have minimum 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character'
+            )
+            .required('Password is required'),
+    });
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            // Retrieve data from local storage
+            const savedData = JSON.parse(localStorage.getItem("formData"));
+
+            if (savedData && savedData.email === values.email && savedData.password === values.password) {
+                alert("Logged in successfully!");
+            } else {
+                alert("Invalid email or password");
+            }
+        },
+    });
+    return (
+        <Contain>
+            <LoginContainer>
+                <Heading>Welcome</Heading>
+                <WelcomeText>Login</WelcomeText>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="login-form">
+                        <InputField
+                            type="email"
+                            placeholder="user email"
+                            id="email"
+                            name="email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                            <div className="text-center text-danger">{formik.errors.email}</div>
+                        ) : null}
+                        <InputField
+                            type="password"
+                            placeholder="Password"
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}
+                        />
+                        {formik.touched.password && formik.errors.password ? (
+                            <div className="text-center text-danger">{formik.errors.password}</div>
+                        ) : null}
+                        <LoginButton type="submit">Login</LoginButton>
+                    </div>
+                    <p className="text-center">New user?</p>
+                    <LoginButton type="button">Register</LoginButton>
+                </form>
+            </LoginContainer>
+        </Contain>
+    );
 };
 
 export default Login;
